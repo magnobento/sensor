53f3e56f13968b0472e5c84a39cccc8633db3c50
magnobento <67931559+magnobento@users.noreply.github.com>
2026-03-01 17:23:57 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo "Starting factory bootstrap..."
 apt update
 apt install -y ansible
 
-cd /opt/sensor
+cd /opt/factory
 ansible-playbook -i localhost, -c local playbook.yml
 
 echo "Factory bootstrap finished."
